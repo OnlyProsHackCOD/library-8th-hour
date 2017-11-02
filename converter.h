b862213e229cd86cf273fb28faf4e9b61de55523
OnlyProsHackCOD <31670244+OnlyProsHackCOD@users.noreply.github.com>
2017-11-02 15:05:32 -0500

    Update converter.h

--- converter.h
+++ converter.h
@@ -6,3 +6,11 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+float cm_to_miles (void)
+{
+    //HUNTER HOWLAND
+    float cm= get_float ("Number of centimeters:");
+    float m= cm*0.00000621371;
+    printf("\nThe number of miles is %.2f\n",m);
+    return m;
+}
